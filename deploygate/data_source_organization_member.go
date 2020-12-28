8bf0bb9f61a13fc03d7a86cb9d12abb7507fd3c0
fnaoto <f.naoto832+github@gmail.com>
2020-12-28 09:59:13 +0900

    Add data resource for organization member.

---
+++ deploygate/data_source_organization_member.go
@@ -0,0 +1,71 @@
+package deploygate
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	go_deploygate "github.com/recruit-mp/go-deploygate"
+)
+
+func dataSourceOrganizationMember() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceOrganizationMemberRead,
+
+		Schema: map[string]*schema.Schema{
+			"organization": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"members": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"username": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"email": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"icon_url": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"inviting": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceOrganizationMemberRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*Client).client
+
+	organization := d.Get("organization").(string)
+
+	log.Printf("[DEBUG] dataSourceOrganizationMemberRead: %s", organization)
+
+	g := &go_deploygate.GetOrganizationMemberInput{
+		OrganizationName: organization,
+	}
+
+	om, err := client.GetOrganizationMember(g)
+
+	if err != nil {
+		return err
+	}
+
+	rs := om
+
+	d.SetId(fmt.Sprintf("%s", organization))
+	d.Set("members", rs.Members)
+
+	return nil
+}
