70db954bb686729cc9ce924422729c80f2d43ac9
fnaoto <f.naoto832+github@gmail.com>
2022-06-08 13:57:34 +0900

    Fix provider test on pre check.

--- deploygate/provider_test.go
+++ deploygate/provider_test.go
@@ -20,6 +20,18 @@ var testDGProviderConfigure sync.Once
 var testDGProviders map[string]*schema.Provider
 
 func Test_DGPreCheck(t *testing.T) {
+	// Test for config
+	configRaw := map[string]interface{}{
+		"api_key": "dummy",
+	}
+	testDGProviderConfigure.Do(func() {
+		err := testDGProvider.Configure(context.Background(), terraform.NewResourceConfigRaw(configRaw))
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	// Test for environment variables
 	testDGProviderConfigure.Do(func() {
 		t.Setenv("DG_API_KEY", "dummy")
 		err := testDGProvider.Configure(context.Background(), terraform.NewResourceConfigRaw(nil))
