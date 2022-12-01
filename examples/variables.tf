3c8cefbbcf7274fc6195b0f2ad190beb6e2ff5fb
fnaoto <f.naoto832+github@gmail.com>
2022-12-01 14:20:26 +0900

    Add sample for organization team member.

---
+++ examples/variables.tf
@@ -0,0 +1,15 @@
+variable "enterprise" {
+  type = string
+}
+
+variable "organization" {
+  type = string
+}
+
+variable "team" {
+  type = string
+}
+
+variable "add_member_name" {
+  type = string
+}
