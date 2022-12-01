bfafe81d1a38282a98081364c45508a873d3239e
fnaoto <f.naoto832+github@gmail.com>
2022-12-01 17:01:11 +0900

    Generate docs with tfplugindocs.

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
+//go:generate tfplugindocs
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: deploygate.Provider,
